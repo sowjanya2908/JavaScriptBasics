352b4e38efb6f56bc416fb3baf4a1df343fc0512
sowjanya2908 <sowjichava5@gmail.com>
2026-03-09 23:37:38 +0530

    feat(ElectricityBill):add the if condition on the calulate Bill

--- JavaScript/ElectricityBill.js
+++ JavaScript/ElectricityBill.js
@@ -7,7 +7,17 @@ function calculateBill() {
         return;
     }
 
-    
+    if (units <= 100) {
+        amount = units * 1.5;
+    } else if (units <= 200) {
+        amount = 100 * 1.5 + (units - 100) * 2.0;
+    } else if (units <= 500) {
+        amount = 100 * 1.5 + 100 * 2.0 + (units - 200) * 3.0;
+    } else if (units <= 1000) {
+        amount = 100 * 1.5 + 100 * 2.0 + 300 * 3.0 + (units - 500) * 4.0;
+    } else {
+        amount = 100 * 1.5 + 100 * 2.0 + 300 * 3.0 + 500 * 4.0 + (units - 1000) * 5.0;
+    }
 
     document.getElementById('result').innerText = `Total Amount: $${amount.toFixed(2)}`;
 }
