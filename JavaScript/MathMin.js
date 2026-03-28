d487006340795809cd860e23e9cc8b193a364a11
sowjanya2908 <sowjichava5@gmail.com>
2026-03-28 13:19:16 +0530

    fear(MathMin):add  the functional of min math value

---
+++ JavaScript/MathMin.js
@@ -0,0 +1,11 @@
+function onClickFindMin() {
+    debugger;
+    var digits = document.getElementById("txtDigits").value;
+    var value1 = Math.pow(10, digits)*Math.random();
+    var value2 = Math.pow(10, digits)*Math.random();
+    var value3 = Math.pow(10, digits)*Math.random();
+    var value4 = Math.pow(10, digits)*Math.random();
+    var value5 = Math.pow(10, digits)* Math.random();
+    var minValue = Math.min(value1, value2, value3, value4, value5);
+    document.getElementById("result").innerHTML = minValue;
+}
