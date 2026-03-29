796136a8bc948d37251e2653c2ddc26fcf9d2359
sowjanya2908 <sowjichava5@gmail.com>
2026-03-29 17:06:53 +0530

    added script and some files

--- JavaScript/MathMax.js
+++ JavaScript/MathMax.js
@@ -1,11 +1,18 @@
-function onClickFindMax() {
-    debugger;
-    var digits = document.getElementById("txtDigits").value;
-    var value1 = Math.pow(10, digits)*Math.random();
-    var value2 = Math.pow(10, digits)*Math.random();
-    var value3 = Math.pow(10, digits)*Math.random();
-    var value4 = Math.pow(10, digits)*Math.random();
-    var value5 = Math.pow(10, digits)* Math.random();
-    var maxValue = Math.max(value1, value2, value3, value4, value5);
-    document.getElementById("result").innerHTML = Math.trunc(maxValue);
-}
+// function onClickFindMax() {
+//     debugger;
+//     var digits = document.getElementById("txtDigits").value;
+//     var value1 = Math.pow(10, digits)*Math.random();
+//     var value2 = Math.pow(10, digits)*Math.random();
+//     var value3 = Math.pow(10, digits)*Math.random();
+//     var value4 = Math.pow(10, digits)*Math.random();
+//     var value5 = Math.pow(10, digits)* Math.random();
+//     var maxValue = Math.max(value1, value2, value3, value4, value5);
+//     document.getElementById("result").innerHTML = Math.trunc(maxValue);
+;
+
+document.getElementById("btnGenerate").addEventListener("click", function() {;
+var sizeNumber = Number(document.getElementById("txtDigits").value);
+var maxValue = Math.pow(10, sizeNumber);
+document.getElementById("result").innerHTML = Math.trunc(Math.random() * maxValue); 
+});
+
