03ae17c85277c9d985811ff7010074d56ef13e18
sowjanya2908 <sowjichava5@gmail.com>
2026-04-02 15:56:06 +0530

    created the add listiners

---
+++ JavaScript/ArithhmeticOperationEventListeners.js
@@ -0,0 +1,32 @@
+document.getElementById("buttonAdd").addEventListener("click", function () {
+        debugger;
+        var value1 = parseInt(document.getElementById("txtValue1").value);
+        var value2 = parseInt(document.getElementById("txtValue2").value);
+        result = value1 + value2;
+        document.getElementById("pResult").innerHTML = result
+
+});
+document.getElementById("buttonSub").addEventListener("click", function () {
+        debugger;
+        var value1 = parseInt(document.getElementById("txtValue1").value);
+        var value2 = parseInt(document.getElementById("txtValue2").value);
+        result = value1 - value2;
+        document.getElementById("pResult").innerHTML = result
+});
+document.getElementById("buttonMultify").addEventListener("click", function () {
+        debugger;
+        var value1 = parseInt(document.getElementById("txtValue1").value);
+        var value2 = parseInt(document.getElementById("txtValue2").value);
+        result = value1 * value2;
+        document.getElementById("pResult").innerHTML = result
+
+});
+document.getElementById("buttonDiv").addEventListener("click", function () {
+        debugger;
+        var value1 = parseInt(document.getElementById("txtValue1").value);
+        var value2 = parseInt(document.getElementById("txtValue2").value);
+        result = value1 / value2;
+        document.getElementById("pResult").innerHTML = result
+});
+
+
